6e70c29b896aa915910fb71349cdec1f2cc0544c
Michaeladze <miswow@yandex.ru>
2017-05-03 18:17:49 +0300

    patch 0.1.0

--- animation.js
+++ animation.js
@@ -1,6 +1,5 @@
 $(document).ready(function () {
 
-
     $(window).scroll(function () {
         var viewportHeight = $(this).innerHeight();
         if ($(this).scrollTop() > viewportHeight / 6) {
@@ -20,10 +19,41 @@ $(document).ready(function () {
         if ($(this).scrollTop() > viewportHeight) {
             $(".logo-appear").css("z-index", "-1");
         }
- 
+        
+        if ($(this).scrollTop() > 2*viewportHeight) {
+            $(".logo-appear").css("display", "none");
+            $(".logo-disappear").css("display", "none");
+        }
+        
+        if ($(this).scrollTop() < 2*viewportHeight) {
+            if ($(this).innerWidth() > 768) {
+                $(".logo-appear").css("display", "block");
+                $(".logo-disappear").css("display", "block");
+            }
+        }
+        
+        if ($(this).scrollTop() > (2*viewportHeight-40)) {
+            $(".navigation").css("background", "rgba(60, 60, 60, .95)");
+        }
+        
+        if ($(this).scrollTop() < (2*viewportHeight-40)) {
+            $(".navigation").css("background", "rgba(60, 60, 60, .2)");
+        }
+        
     });
     
     $(".menu-button").on("click", function() {
         $(".menu ul").toggle(0, "hidden");
-    })
+    });
+    
+    
+    
+    /* Плавные переходы по якорям*/
+    $('a[href*="#"]').bind("click", function(e){    
+        var anchor = $(this);
+        $('html, body').stop().animate({    
+            scrollTop: $(anchor.attr('href')).offset().top - 80
+        }, 1000);    
+        e.preventDefault();    
+    });
 })
---
+++ textilation.js
@@ -0,0 +1,95 @@
+$(document).ready(function () {
+    
+    $(window).on("scroll", function() {
+        var viewportHeight = $(this).innerHeight();
+
+        if ($(this).scrollTop() > 1.3*viewportHeight) {
+            $("#cpf_block .desktop-h1").css("opacity", "1");
+            textilation("#cpf_block");
+        }
+        
+        if ($(this).scrollTop() > 2.3*viewportHeight) {
+            $("#tech-sess_block .desktop-h1").css("opacity", "1");
+            textilation("#tech-sess_block");
+        }
+        
+        if ($(this).scrollTop() > 3.3*viewportHeight) {
+            $("#plenary_block .desktop-h1").css("opacity", "1");
+            textilation("#plenary_block");
+        }
+        
+        if ($(this).scrollTop() > 4.3*viewportHeight) {
+            $("#petroolymic_block .desktop-h1").css("opacity", "1");
+            textilation("#petroolymic_block");
+        }
+        
+        if ($(this).scrollTop() > 5.3*viewportHeight) {
+            $("#culture_block .desktop-h1").css("opacity", "1");
+            textilation("#culture_block");
+        }
+
+        if ($(this).scrollTop() > 6.3*viewportHeight) {
+            $("#excursions_block .desktop-h1").css("opacity", "1");
+            textilation("#excursions_block");
+        }
+
+        if ($(this).scrollTop() > 7.3*viewportHeight) {
+            $("#round-table_block .desktop-h1").css("opacity", "1");
+            textilation("#round-table_block");
+        }
+
+        if ($(this).scrollTop() > 8.3*viewportHeight) {
+            $("#energy4me_block .desktop-h1").css("opacity", "1");
+            textilation("#energy4me_block");
+        }
+
+
+    })
+})
+
+
+function textilation(block) {
+    $(block+' .tlt1').textillate({ in: {
+            effect: "fadeInUpBig",
+            delayScale: 1.2,
+            delay: 120,
+            sync: false,
+            shuffle: true,
+            reverse: false
+        }
+    });
+
+    $(block+' .tlt2').textillate({ in: {
+            effect: "fadeInLeftBig",
+            delayScale: 1.2,
+            delay: 120,
+            sync: false,
+            shuffle: true,
+            reverse: false
+        },
+        callback: function () {
+            
+        }
+    });
+
+    $(block+' .tlt3').textillate({ in: {
+            effect: "fadeInDownBig",
+            delayScale: 1.2,
+            delay: 120,
+            sync: false,
+            shuffle: true,
+            reverse: false
+        }
+    });
+
+    $(block+' .tlt4').textillate({ in: {
+            effect: "fadeInRightBig",
+            delayScale: 1.2,
+            delay: 120,
+            sync: false,
+            shuffle: true,
+            reverse: false
+        },
+
+    });
+}
