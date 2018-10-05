51c0aeddf4a7d085203f704025a4ebfd3f0faa9f
Кутателадзе Михаил Тамазиевич <Kutateladze.M.Ta@sberbank.ru>
2018-10-05 18:34:51 +0300

    m

--- animation.js
+++ animation.js
@@ -14,46 +14,88 @@ $(document).ready(function () {
             $(".logo-disappear").css("z-index", "1");
             $(".logo-disappear").css("opacity", "1");
             $(".logo-appear").css("opacity", "0");
+            $(".up").css("display", "none");
         }
-        
+
         if ($(this).scrollTop() > viewportHeight) {
             $(".logo-appear").css("z-index", "-1");
         }
-        
-        if ($(this).scrollTop() > 2*viewportHeight) {
+
+        if ($(this).scrollTop() > 2 * viewportHeight) {
             $(".logo-appear").css("display", "none");
             $(".logo-disappear").css("display", "none");
         }
-        
-        if ($(this).scrollTop() < 2*viewportHeight) {
-            if ($(this).innerWidth() > 768) {
+
+        if ($(this).scrollTop() < 2 * viewportHeight) {
+            if ($(this).innerWidth() > 1024) {
                 $(".logo-appear").css("display", "block");
                 $(".logo-disappear").css("display", "block");
             }
+            $(".up").css("display", "none");
+            $(".up").css("z-index", "-1");
         }
-        
-        if ($(this).scrollTop() > (2*viewportHeight-40)) {
+
+        if ($(this).scrollTop() > (2 * viewportHeight - 40)) {
             $(".navigation").css("background", "rgba(60, 60, 60, .95)");
+            $(".navigation").css("display", "none");
+            if ($(this).innerWidth() > 1024) {
+                $(".up").css("display", "block");
+                $(".up").css("z-index", "100");
+            }
         }
-        
-        if ($(this).scrollTop() < (2*viewportHeight-40)) {
-            $(".navigation").css("background", "rgba(60, 60, 60, .2)");
+
+        if ($(this).scrollTop() < (2 * viewportHeight - 40)) {
+            if ($(this).innerWidth() > 1024) {
+                $(".navigation").css("background", "rgba(60, 60, 60, .2)");
+                $(".navigation").css("display", "block");
+            }
         }
-        
+
     });
-    
-    $(".menu-button").on("click", function() {
+
+    $(".menu-button").on("click", function () {
         $(".menu ul").toggle(0, "hidden");
     });
-    
-    
-    
+
+    $(".tab").on("click", function () {
+        $(this).css("background", "rgba(82, 90, 136, 1)");
+        $(this).siblings().css("background", "rgba(70, 70, 70, .4)");
+    })
+
+    $(".tab-participant").on("click", function () {
+        $(".content-participant").css("display", "block");
+        $(".content-delegate").css("display", "none");
+        $(".content-guest").css("display", "none");
+
+        $(".registration").css("height", "3700px");
+        $(this).css("background", "rgb(82, 90, 136);");
+    });
+
+    $(".tab-delegate").on("click", function () {
+        $(".content-delegate").css("display", "block");
+        $(".content-participant").css("display", "none");
+        $(".content-guest").css("display", "none");
+
+        $(".registration").css("height", "3700px");
+        $(this).css("background", "rgb(82, 90, 136);");
+    });
+
+    $(".tab-guest").on("click", function () {
+        $(".content-guest").css("display", "block");
+        $(".content-participant").css("display", "none");
+        $(".content-delegate").css("display", "none");
+
+        $(".registration").css("height", "3700px");
+        $(this).css("background", "rgb(82, 90, 136);");
+    });
+
+
     /* Плавные переходы по якорям*/
-    $('a[href*="#"]').bind("click", function(e){    
+    $('a[href*="#"]').bind("click", function (e) {
         var anchor = $(this);
-        $('html, body').stop().animate({    
+        $('html, body').stop().animate({
             scrollTop: $(anchor.attr('href')).offset().top - 80
-        }, 1000);    
-        e.preventDefault();    
+        }, 1000);
+        e.preventDefault();
     });
 })
--- textilation.js
+++ textilation.js
@@ -2,42 +2,43 @@ $(document).ready(function () {
     
     $(window).on("scroll", function() {
         var viewportHeight = $(this).innerHeight();
-
-        if ($(this).scrollTop() > 1.3*viewportHeight) {
-            $("#cpf_block .desktop-h1").css("opacity", "1");
-            textilation("#cpf_block");
-        }
         
-        if ($(this).scrollTop() > 2.3*viewportHeight) {
+        if ($(this).scrollTop() > 1.3*viewportHeight) {
             $("#tech-sess_block .desktop-h1").css("opacity", "1");
             textilation("#tech-sess_block");
         }
         
-        if ($(this).scrollTop() > 3.3*viewportHeight) {
+        if ($(this).scrollTop() > 2.3*viewportHeight) {
             $("#plenary_block .desktop-h1").css("opacity", "1");
             textilation("#plenary_block");
         }
         
+        if ($(this).scrollTop() > 3.3*viewportHeight) {
+            $("#round-table_block .desktop-h1").css("opacity", "1");
+            textilation("#round-table_block");
+        }
+        
         if ($(this).scrollTop() > 4.3*viewportHeight) {
+            $("#oilrush_block .desktop-h1").css("opacity", "1");
+            textilation("#oilrush_block");
+        }
+        
+        if ($(this).scrollTop() > 5.3*viewportHeight) {
             $("#petroolymic_block .desktop-h1").css("opacity", "1");
             textilation("#petroolymic_block");
         }
         
-        if ($(this).scrollTop() > 5.3*viewportHeight) {
+        if ($(this).scrollTop() > 6.3*viewportHeight) {
             $("#culture_block .desktop-h1").css("opacity", "1");
             textilation("#culture_block");
         }
 
-        if ($(this).scrollTop() > 6.3*viewportHeight) {
-            $("#excursions_block .desktop-h1").css("opacity", "1");
-            textilation("#excursions_block");
-        }
-
         if ($(this).scrollTop() > 7.3*viewportHeight) {
-            $("#round-table_block .desktop-h1").css("opacity", "1");
-            textilation("#round-table_block");
+            $("#tech_block .desktop-h1").css("opacity", "1");
+            textilation("#tech_block");
         }
 
+        
         if ($(this).scrollTop() > 8.3*viewportHeight) {
             $("#energy4me_block .desktop-h1").css("opacity", "1");
             textilation("#energy4me_block");
@@ -51,7 +52,7 @@ $(document).ready(function () {
 function textilation(block) {
     $(block+' .tlt1').textillate({ in: {
             effect: "fadeInUpBig",
-            delayScale: 1.2,
+            delayScale: 1,
             delay: 120,
             sync: false,
             shuffle: true,
@@ -61,7 +62,7 @@ function textilation(block) {
 
     $(block+' .tlt2').textillate({ in: {
             effect: "fadeInLeftBig",
-            delayScale: 1.2,
+            delayScale: 1,
             delay: 120,
             sync: false,
             shuffle: true,
@@ -74,7 +75,7 @@ function textilation(block) {
 
     $(block+' .tlt3').textillate({ in: {
             effect: "fadeInDownBig",
-            delayScale: 1.2,
+            delayScale: 1,
             delay: 120,
             sync: false,
             shuffle: true,
@@ -84,8 +85,8 @@ function textilation(block) {
 
     $(block+' .tlt4').textillate({ in: {
             effect: "fadeInRightBig",
-            delayScale: 1.2,
-            delay: 120,
+            delayScale: .1,
+            delay: 10,
             sync: false,
             shuffle: true,
             reverse: false
